083ed761a2fce5bcd13036750cca2e8ec51477f8
MathApc <mat.apcher@gmail.com>
2021-06-09 12:37:01 +0200

    commentaires + bdd

--- boutique.php
+++ boutique.php
@@ -1,5 +1,5 @@
 <?php 
-            session_start();
+            session_start(); //inclut les variables de session
 	 		      $servname = 'localhost';
             $dbname = 'projetinfo1';
             $user = 'root';
@@ -28,7 +28,7 @@
     <meta charset="utf-8">
     <title>Boutique</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="css/accueil.css">
+    <link rel="stylesheet" type="text/css" href="css/accueil.css"> <! -- design de la page (bootstrap)  -->
     <link rel="stylesheet" type="text/css" href="css/footer.css">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
   </head>
@@ -39,18 +39,18 @@ include 'menu.php';
 <div class="container" style="margin-bottom: 150px;">
     <div class="row justify-content-center">
         <h1 class="text-center title-top"> La boutique </h1>
-        <?php $sql =  'select * from produit';
-				foreach  ($dbco->query($sql) as $row) { ?>
+        <?php $sql =  'select * from produit'; 
+				foreach  ($dbco->query($sql) as $row) { //boucle pour afficher exactement le nombre de produit présents dans la bdd?>
         
         <div class="col" style="margin-left: 60px" id="<?php echo($row['nom']) ?>">
-            <form action="produit.php" method="post" name="<?php echo($row['id']) ?>">
+            <form action="produit.php" method="post" name="<?php echo($row['id']) //aller dans produit.php avec le bon produit via l'id?>">
                 <div class="card" style="width: 18rem;">
-                    <img src="<?php echo($row['lien_image']) ?>" class="card-img-top" alt="...">
+                    <img src="<?php echo($row['lien_image']) //affichage de l'image ?>" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title"><?php echo($row['nom']) ?></h5>
-                        <p class="card-text"><?php echo($row['prix']) ?>€</p>
-                        <p class="card-text">Stock : <?php echo($row['stock']) ?></p>
-                        <button type="submit" class="btn btn-primary" name="<?php echo($row['id']) ?>" id="<?php echo($row['id']) ?>">Acheter</button>
+                        <h5 class="card-title"><?php echo($row['nom']) //affichage du nom du produit ?></h5>
+                        <p class="card-text"><?php echo($row['prix']) //affichage du prix (en euro) ?>€</p>
+                        <p class="card-text">Stock : <?php echo($row['stock']) //affichage du stock?></p>
+                        <button type="submit" class="btn btn-primary" name="<?php echo($row['id']) ?>" id="<?php echo($row['id']) //on met des valeur dans le bouton pour pouvoir acheter le produit (en fonction du bouton ou l'on a cliqué)?>">Acheter</button>
                     </div>
                 </div>
             </form>
--- projetinfo1.sql
+++ projetinfo1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : Dim 06 juin 2021 à 14:18
+-- Généré le : mer. 09 juin 2021 à 10:33
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.3.21
 
@@ -32,13 +32,13 @@ CREATE TABLE IF NOT EXISTS `evenements` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(100) NOT NULL,
   `prix` varchar(10) NOT NULL,
-  `dateDebut` date DEFAULT NULL,
-  `lien_image` varchar(100) DEFAULT NULL,
+  `dateDebut` datetime DEFAULT NULL,
+  `lien_image` varchar(100) NOT NULL,
   `description` text NOT NULL,
   `lieu` text NOT NULL,
-  `dateFin` date DEFAULT NULL,
+  `dateFin` datetime DEFAULT NULL,
   `nombreParticipants` int(10) NOT NULL,
-  `dateLimiteInscription` date DEFAULT NULL,
+  `dateLimiteInscription` datetime DEFAULT NULL,
   `placesRestantes` int(10) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
@@ -48,9 +48,9 @@ CREATE TABLE IF NOT EXISTS `evenements` (
 --
 
 INSERT INTO `evenements` (`id`, `nom`, `prix`, `dateDebut`, `lien_image`, `description`, `lieu`, `dateFin`, `nombreParticipants`, `dateLimiteInscription`, `placesRestantes`) VALUES
-(1, 'Uno nouvelle generation', '2', '2021-06-10', 'images/uno.jpeg', '', 'CY 201', '2021-06-10', 20, '2021-06-09', 20),
-(2, 'Jeu de role', '5', '2021-06-08', 'images/loupGarou.jpeg', '', 'TG 201', '2021-06-08', 30, '2021-06-07', 29),
-(3, 'Jeu de role ', '20', '2021-06-24', 'images/donjonDragon.jpg', '', 'CT 201', '2021-06-24', 10, '2021-06-23', 10);
+(1, 'Uno nouvelle generation', '2', '2021-06-10 17:00:00', 'images/uno.jpeg', 'Nous organisons des parties de Uno sous forme de tournois par quatre. Celui-ci ce deroulera en 4 parties. Les regles seront celles de bases, avec l\'ajout de la volee et d\'une bonne pioche.', 'CY 201', '2021-06-10 19:45:00', 20, '2021-06-09 23:59:59', 19),
+(2, 'Loup-garou', '5', '2021-06-08 17:00:00', 'images/loupGarou.jpeg', 'Tout le monde connait ce guessing incontournable qu\'est le loup-garou, venez vous amuser, tout les coups seront permis.', 'TG 201', '2021-06-08 19:30:00', 30, '2021-06-07 23:59:59', 27),
+(3, 'Donjons et Dragons', '20', '2021-06-24 17:30:00', 'images/donjonDragon.jpg', 'Nous voulons vous faire decouvrir un univers unique du Role play plateau, un classique dans ce genre, Donjons et Dragons ont conquis bien des coeurs, a vous de le faire.', 'CT 201', '2021-06-24 19:45:00', 10, '2021-06-23 23:59:59', 10);
 
 -- --------------------------------------------------------
 
@@ -119,7 +119,7 @@ CREATE TABLE IF NOT EXISTS `reservation` (
   `date_ajout` date NOT NULL,
   `montant` int(10) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `reservation`
@@ -127,7 +127,10 @@ CREATE TABLE IF NOT EXISTS `reservation` (
 
 INSERT INTO `reservation` (`id`, `idEvenements`, `idUtilisateur`, `date_ajout`, `montant`) VALUES
 (1, 2, 6, '2021-06-06', 5),
-(2, 2, 6, '2021-06-06', 5);
+(2, 2, 6, '2021-06-06', 5),
+(3, 2, 6, '2021-06-06', 5),
+(4, 2, 6, '2021-06-06', 5),
+(5, 1, 6, '2021-06-07', 2);
 
 -- --------------------------------------------------------
 
@@ -165,7 +168,7 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `mail` varchar(100) NOT NULL,
   `mdp` varchar(100) NOT NULL,
   `pseudo` varchar(100) NOT NULL,
-  `idtypeUtilisateurs` int(10) NOT NULL,
+  `idtypeUtilisateurs` int(10) NOT NULL DEFAULT '2',
   `adresse` varchar(100) DEFAULT NULL,
   `ville` varchar(100) DEFAULT NULL,
   `codePostale` int(10) DEFAULT NULL,
@@ -181,7 +184,7 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
 
 INSERT INTO `utilisateurs` (`id`, `nom`, `prenom`, `age`, `mail`, `mdp`, `pseudo`, `idtypeUtilisateurs`, `adresse`, `ville`, `codePostale`, `telephone`) VALUES
 (1, 'ap', 'mathéo', 20, 'test@gmail.com', 'test', 'test', 1, '16 RUE DE LA FORGE', 'ST LEU LA FORET', 95320, '0672291212'),
-(6, 'apcher', 'matheo', 21, 'mat.apcher@gmail.com', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', 'Aki', 1, '00400', 'St Leu La ForÃªt', 95320, '07 82 16 71 47');
+(6, 'apcher', 'matheo', 22, 'mat.apcher@gmail.com', '8cb2237d0679ca88db6464eac60da96345513964', 'Aki', 1, '00400', 'St Leu La ForÃªt', 95320, '07 82 16 71 47');
 
 --
 -- Contraintes pour les tables déchargées
